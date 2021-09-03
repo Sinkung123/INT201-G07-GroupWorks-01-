7bf154fbddd4b76dc26b59c2c7dd37c4a387f3f3
unknown <naphatsawan.bw@gmail.com>
2021-09-04 00:33:26 +0700

    fix the method

--- HW02/HW02_quiz.js
+++ HW02/HW02_quiz.js
@@ -1,51 +1,41 @@
-const questions = {
-    question1: { q: 'What is 7 times 3', 
-                c: { a: 19, b: 20, c: 21, d: 22 },
-                a: 'c'
-    },
-    question2: { q: 'Which one is different from the others?', 
-                c: { 1: 'HTML', 2: 'JavaScript', 3: 'PHP', 4: 'C++' },
-                a: 1
-    },
-    question3: { q: 'Which one is a mammal?', 
-                c: { 1: 'Crocodile', 2: 'Shark', 3: 'Dolphin', 4: 'Ant' },
-                a: 3
-    },
-    question4: { q: 'when was microsoft established?', 
-                c: { 1: 2021, 2: 1987, 3: 1999, 4: 1975 },
-                a: 4
-    },
-    question5: { q: 'What is root of 121', 
-                c: { 1: 10, 2: 11, 3: 12, 4: 13 },
-                a: 2
+class questions{
+    constructor(question, choice, answer){
+        this._question = question;
+        this._choice = choice;
+        this._answer = answer;
     }
 }
-
 class Player{
     constructor(name, chosenQ, chosenAns, points){
-        this._name = name;
-        this._chosenQ = chosenQ;
-        this._chosenAns = chosenAns;
-        this._points = points;
+        this._name = name; // player name
+        this._chosenQ = chosenQ; // object of the chosen question
+        this._chosenAns = chosenAns; // player's answer
+        this._points = points; // starting point
     }
     checkAnswer(){
-        if(questions.question1.c == questions.question1.a){
+      let correctAns = this._chosenQ._choice[this._chosenQ._answer]
+        if(correctAns === this._chosenAns){
             Player.points += 1;
             return 'Correct!';
         }
         return 'Wrong!';
     }
 }
+let q1 = new questions('What is 7 times 3', { 1: 19, 2: 20, 3: 21, 4: 22 },3);
+let q2 = new questions('Which one is different from the others?', { 1: 'HTML', 2: 'JavaScript', 3: 'PHP', 4: 'C++' }, 1);
+let q3 = new questions('Which one is a mammal?', { 1: 'Crocodile', 2: 'Shark', 3: 'Dolphin', 4: 'Ant' }, 3);
+let q4 = new questions('when was microsoft established?', { 1: 2021, 2: 1987, 3: 1999, 4: 1975 }, 4);
+let q5 = new questions('What is root of 121', { 1: 10, 2: 11, 3: 12, 4: 13 }, 2);
 
-let A = new Player('A', questions["question1"], questions['question1']['c']['3']);
+let A = new Player('A',
+                   q1,
+                   22,
+                   0
+                  );
 console.log(A.checkAnswer());
-
-
-
-// function checkAnswer(Player){
-//     if(Player.questions["chosenQ"]["chosenAns"] == Player.questions[chosenQ][a]){
-//         Player.points += 1;
-//         return 'Correct!';
-//     }
-//     return 'Wrong!';
-// }
+let B = new Player('B',
+                   q1,
+                   21,
+                   0
+                  );
+console.log(B.checkAnswer());
