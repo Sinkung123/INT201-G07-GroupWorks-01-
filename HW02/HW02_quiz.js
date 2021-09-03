7f1bfe8873d2bd86f49b169ba03de7e1a506995a
Purachet Zheng <poorachad2545@gmail.com>
2021-09-04 00:53:34 +0700

    doQuiz

--- HW02/HW02_quiz.js
+++ HW02/HW02_quiz.js
@@ -6,36 +6,31 @@ class questions{
     }
 }
 class Player{
-    constructor(name, chosenQ, chosenAns, points){
+    constructor(name){
         this._name = name; // player name
-        this._chosenQ = chosenQ; // object of the chosen question
-        this._chosenAns = chosenAns; // player's answer
-        this._points = points; // starting point
+        this._points = 0; // starting point
     }
-    checkAnswer(){
-      let correctAns = this._chosenQ._choice[this._chosenQ._answer]
-        if(correctAns === this._chosenAns){
-            Player.points += 1;
+    doQuiz(num, choose){
+        if(questionsList[num-1]._answer === choose){
+            this._points +=1;
             return 'Correct!';
         }
         return 'Wrong!';
     }
 }
-let q1 = new questions('What is 7 times 3', { 1: 19, 2: 20, 3: 21, 4: 22 },3);
-let q2 = new questions('Which one is different from the others?', { 1: 'HTML', 2: 'JavaScript', 3: 'PHP', 4: 'C++' }, 1);
-let q3 = new questions('Which one is a mammal?', { 1: 'Crocodile', 2: 'Shark', 3: 'Dolphin', 4: 'Ant' }, 3);
-let q4 = new questions('when was microsoft established?', { 1: 2021, 2: 1987, 3: 1999, 4: 1975 }, 4);
-let q5 = new questions('What is root of 121', { 1: 10, 2: 11, 3: 12, 4: 13 }, 2);
 
-let A = new Player('A',
-                   q1,
-                   22,
-                   0
-                  );
-console.log(A.checkAnswer());
-let B = new Player('B',
-                   q1,
-                   21,
-                   0
-                  );
-console.log(B.checkAnswer());
+let questionsList = new Array();
+questionsList[0] = new questions('What is 7 times 3', { 1: 19, 2: 20, 3: 21, 4: 22 },3);
+questionsList[1] = new questions('Which one is different from the others?', { 1: 'HTML', 2: 'JavaScript', 3: 'PHP', 4: 'C++' }, 1);
+questionsList[2] = new questions('Which one is a mammal?', { 1: 'Crocodile', 2: 'Shark', 3: 'Dolphin', 4: 'Ant' }, 3);
+questionsList[3] = new questions('when was microsoft established?', { 1: 2021, 2: 1987, 3: 1999, 4: 1975 }, 4);
+questionsList[4] = new questions('What is root of 121', { 1: 10, 2: 11, 3: 12, 4: 13 }, 2);
+
+let A = new Player('A');
+console.log(A.doQuiz(1,3));
+console.log(A.doQuiz(2,1));
+
+
+
+let B = new Player('B');
+console.log(B.doQuiz(1,2));
