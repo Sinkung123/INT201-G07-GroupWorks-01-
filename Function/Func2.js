5bb8143173b5002c998a8a4b5c24239ac9487e94
pepoppe <72536699+Sinkung123@users.noreply.github.com>
2021-10-18 21:59:14 +0700

    Update Func2.js

--- Function/Func2.js
+++ Function/Func2.js
@@ -1,2 +1,10 @@
 
 import  {studentList,restPara} from './Func.js';
+
+let list = studentList("1","bob")
+console.log(list("2","peter"))
+console.log(list("3","Joey"))
+
+console.log(restPara(1,2,3,4))
+console.log(restPara([1,2],[10,5],{id: '123'}))
+console.log(restPara("sin",[1,2,3,4],{ name : "peter" ,sur: "smith"}))
