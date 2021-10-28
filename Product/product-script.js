d4879de0b8e0bb76bfa65abe8a68639f676040a4
NaruebetP <naruebetr.p@gmail.com>
2021-10-28 17:57:29 +0700

    Updated CSS

--- Product/product-script.js
+++ Product/product-script.js
@@ -1,34 +1,39 @@
-import {products} from './product.js';
-const divProductsEle = document.querySelector('#products');
-divProductsEle.setAttribute('class', 'container row')
-for(let product of products){
-    const divProductEle = document.createElement('div');
-    divProductEle.setAttribute('id', product.productId);
-    divProductEle.setAttribute('class', 'col-3');
-    divProductEle.setAttribute('style', 'border: red 1px solid')
-    
-    const pProductImgEle = document.createElement('img');
-    pProductImgEle.setAttribute('class', 'justify-content-center')
-    pProductImgEle.setAttribute('src', product.img);
-    pProductImgEle.setAttribute('width', 200);
-    pProductImgEle.setAttribute('height', 200);
-    divProductEle.appendChild(pProductImgEle);
-    
-    const pProductIdEle = document.createElement('p');
-    pProductIdEle.textContent = product.productId;
-    divProductEle.appendChild(pProductIdEle);
+import { products } from "./product.js";
+console.log(products[0].productId);
+const divProductsEle = document.querySelector("#products");
+divProductsEle.setAttribute("class", "container row justify-content-center mx-auto");
 
-    const pProductNameEle = document.createElement('p');
-    pProductNameEle.textContent = product.productName;
-    divProductEle.appendChild(pProductNameEle);
+for (let product of products) {
+  const divProductEle = document.createElement("div");
+  divProductEle.setAttribute("id", product.productId);
+  divProductEle.setAttribute("class", "container col-4 mx-auto");
+  divProductEle.setAttribute("style", "margin-bottom: 50px; text-align: center;")
 
-    const pProductPriceEle = document.createElement('p');
-    pProductPriceEle.textContent = product.price + ' Baht';
-    divProductEle.appendChild(pProductPriceEle);
+  const pProductImgEle = document.createElement("img");
+  pProductImgEle.setAttribute("src", product.img);
+  pProductImgEle.setAttribute("height", 200);
+  divProductEle.appendChild(pProductImgEle);
 
-    const pProductStockEle = document.createElement('p');
-    pProductStockEle.textContent = product.stock;
-    divProductEle.appendChild(pProductStockEle);
+  const pProductIdEle = document.createElement("p");
+  pProductIdEle.textContent = product.productId;
+  divProductEle.appendChild(pProductIdEle);
 
-    divProductsEle.appendChild(divProductEle);
+  const pProductNameEle = document.createElement("p");
+  pProductNameEle.textContent = product.productName;
+  divProductEle.appendChild(pProductNameEle);
+
+  const pProductPriceEle = document.createElement("p");
+  pProductPriceEle.textContent = product.price + " Baht";
+  divProductEle.appendChild(pProductPriceEle);
+  
+  const pProductStockEle = document.createElement('p');
+  pProductStockEle.textContent = "Available : " + product.stock;
+  divProductEle.appendChild(pProductStockEle);
+
+  const pProductBuyEle = document.createElement("button");
+  pProductBuyEle.setAttribute("type", "button");
+  pProductBuyEle.setAttribute("class", "btn btn-primary");
+  pProductBuyEle.textContent = "Buy Now";
+  divProductEle.appendChild(pProductBuyEle);
+  divProductsEle.appendChild(divProductEle);
 }
--- Product/product.js
+++ Product/product.js
@@ -1,109 +1,49 @@
-export let products = [{
-
-
-
-    img: 'https://cdn.discordapp.com/attachments/882934034718670873/903215376354119751/2288-111-1_1_4.png',
-
-
-
-    productId: 'S001',
-
-
-
-    productName: 'NIKE AIR FORCE ONE',
-
-
-
-    price: '฿3,500'}
-
-
-
-    ,{
-
-
-
-    img: 'https://cdn.discordapp.com/attachments/882934034718670873/903215599902146570/ml574ba2-1.png',
-
-
-
-    productId: 'S002',
-
-
-
-    productName: 'NEW BALANCE 574',
-
-
-
-    price: '฿2,990'}
-
-    ,{
-
-   
-
-    img: 'https://cdn.discordapp.com/attachments/882934034718670873/903216827868540948/da7995-600-1.png',
-
-
-
-    productId: 'S003',
-
-
-
-    productName: 'NIKE WAFFLE ONE',
-
-
-
-    price: '฿2,340'}
-
-    ,{
-
-    img: 'https://cdn.discordapp.com/attachments/882934034718670873/903217438181720114/gz7922-1.png',
-
-   
-
-    productId: 'S004',
-
-   
-
-    productName: 'ADIDAS NMD_R1',
-
-   
-
-    price: '฿4,600'}
-
-    ,{
-
-
-
-    img: 'https://cdn.discordapp.com/attachments/882934034718670873/903217856135725076/E0B8A3E0B8ADE0B887E0B980E0B897E0B989E0B8B2E0B980E0B897E0B8A3E0B899E0B899E0B8B4E0B988E0B887E0B89CE0B8B9E0B989-air-monarch-4-cc3XbD.png',
-
-       
-
-    productId: 'S005',
-
-       
-
-    productName: 'Nike Air Monarch IV',
-
-       
-
-    price: '฿1,900'}
-
-    ,{
-
-    img: 'https://cdn.discordapp.com/attachments/882934034718670873/903219510251761694/E0B8A3E0B8ADE0B887E0B980E0B897E0B989E0B8B2E0B8A7E0B8B4E0B988E0B887E0B982E0B8A3E0B989E0B894E0B8A3E0B8B1E0B899E0B899E0B8B4E0B988E0B887E0B89CE0B8B9E0B989-react-infinity-run-flyknit-2-Qfh63N.png',
-
-       
-
-    productId: 'S006',
-
-       
-
-    productName: 'Nike React Infinity Run Flyknit 2',
-
-       
-
-    price: '฿5,800'}  
-
-
-
-]
+export let products = [
+  {
+    img: "https://cdn.discordapp.com/attachments/882934034718670873/903215376354119751/2288-111-1_1_4.png",
+    productId: "S001",
+    productName: "NIKE AIR FORCE ONE",
+    price: "3,500",
+    stock: 0,
+  },
+
+  {
+    img: "https://cdn.discordapp.com/attachments/882934034718670873/903215599902146570/ml574ba2-1.png",
+    productId: "S002",
+    productName: "NEW BALANCE 574",
+    price: "2,990",
+    stock: 2,
+  },
+
+  {
+    img: "https://cdn.discordapp.com/attachments/882934034718670873/903216827868540948/da7995-600-1.png",
+    productId: "S003",
+    productName: "NIKE WAFFLE ONE",
+    price: "2,340",
+    stock: 3,
+  },
+
+  {
+    img: "https://cdn.discordapp.com/attachments/882934034718670873/903217438181720114/gz7922-1.png",
+    productId: "S004",
+    productName: "ADIDAS NMD_R1",
+    price: "4,600",
+    stock: 10,
+  },
+
+  {
+    img: "https://cdn.discordapp.com/attachments/882934034718670873/903217856135725076/E0B8A3E0B8ADE0B887E0B980E0B897E0B989E0B8B2E0B980E0B897E0B8A3E0B899E0B899E0B8B4E0B988E0B887E0B89CE0B8B9E0B989-air-monarch-4-cc3XbD.png",
+    productId: "S005",
+    productName: "Nike Air Monarch IV",
+    price: "1,900",
+    stock: 0,
+  },
+
+  {
+    img: "https://cdn.discordapp.com/attachments/882934034718670873/903219510251761694/E0B8A3E0B8ADE0B887E0B980E0B897E0B989E0B8B2E0B8A7E0B8B4E0B988E0B887E0B982E0B8A3E0B989E0B894E0B8A3E0B8B1E0B899E0B899E0B8B4E0B988E0B887E0B89CE0B8B9E0B989-react-infinity-run-flyknit-2-Qfh63N.png",
+    productId: "S006",
+    productName: "Nike React Infinity Run Flyknit 2",
+    price: "5,800",
+    stock: 9,
+  },
+];
