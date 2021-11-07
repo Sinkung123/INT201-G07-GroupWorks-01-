29b9d04223042085a7c77f901ce6f8ed3edc345e
Purachet Zheng <poorachad2545@gmail.com>
2021-11-07 22:16:41 +0700

    createproductyame() to arrow function

--- Product v.1.2.1/product-search.js
+++ Product v.1.2.1/product-search.js
@@ -51,52 +51,51 @@ function SearchIconBtn(){
 
 
 
-function createProductByName(search){
-    for (let product of products) {
-        if(product.productName.toLowerCase().match(search.toLowerCase())){
-            const divProductEle = document.createElement('div');
-            divProductEle.setAttribute('id', product.productId);
-            divProductEle.setAttribute('class', 'col-4 card border-0 mb-4 mx-2 p-0');
-            divProductEle.setAttribute('style', 'cursor: pointer; width: 25em;')
+let createProductByName = (search) => products.forEach(product => {
+    if(product.productName.toLowerCase().match(search.toLowerCase())){
+        const divProductEle = document.createElement('div');
+        divProductEle.setAttribute('id', product.productId);
+        divProductEle.setAttribute('class', 'col-4 card border-0 mb-4 mx-2 p-0');
+        divProductEle.setAttribute('style', 'cursor: pointer; width: 25em;')
 
-            const pProductImgEle = document.createElement('img');
-            pProductImgEle.setAttribute('src', product.img);
-            pProductImgEle.setAttribute('class', 'card-img-top');
-            divProductEle.appendChild(pProductImgEle);
-          
-            const pProductIdEle = document.createElement('h5');
-            pProductIdEle.textContent = product.productId;
-            pProductIdEle.setAttribute('class', 'card-title text-center');
-            // pProductIdEle.setAttribute('style', 'margin: -40px 0px 30px;');
-            pProductIdEle.setAttribute('style', 'margin: -380px 0px 370px;');
-            divProductEle.appendChild(pProductIdEle);
-          
-            const pProductNameEle = document.createElement('h5');
-            pProductNameEle.textContent = product.productName;
-            pProductNameEle.setAttribute('class', 'card-title');
-            divProductEle.appendChild(pProductNameEle);
-          
-            const pProductPriceEle = document.createElement('h6');
-            pProductPriceEle.textContent = product.productPrice + ' Baht';
-            pProductPriceEle.setAttribute('class', 'card-text text-secondary');
-            divProductEle.appendChild(pProductPriceEle);
-            
-            const pProductStockEle = document.createElement('h6');
-            pProductStockEle.textContent = 'Available : ' + product.productStock;
-            pProductStockEle.setAttribute('class', 'card-text text-secondary');
-            divProductEle.appendChild(pProductStockEle);
-          
-            // const pProductBuyEle = document.createElement('button');
-            // pProductBuyEle.setAttribute('type', 'button');
-            // pProductBuyEle.setAttribute('class', 'btn btn-primary');
-            // pProductBuyEle.textContent = 'Buy Now';
-            // pProductBuyEle.onclick = function() {}
-            // divProductEle.appendChild(pProductBuyEle);
-          
-            divProductsEle.appendChild(divProductEle);
-        }
+        const pProductImgEle = document.createElement('img');
+        pProductImgEle.setAttribute('src', product.img);
+        pProductImgEle.setAttribute('class', 'card-img-top');
+        divProductEle.appendChild(pProductImgEle);
+      
+        const pProductIdEle = document.createElement('h5');
+        pProductIdEle.textContent = product.productId;
+        pProductIdEle.setAttribute('class', 'card-title text-center');
+        // pProductIdEle.setAttribute('style', 'margin: -40px 0px 30px;');
+        pProductIdEle.setAttribute('style', 'margin: -380px 0px 370px;');
+        divProductEle.appendChild(pProductIdEle);
+      
+        const pProductNameEle = document.createElement('h5');
+        pProductNameEle.textContent = product.productName;
+        pProductNameEle.setAttribute('class', 'card-title');
+        divProductEle.appendChild(pProductNameEle);
+      
+        const pProductPriceEle = document.createElement('h6');
+        pProductPriceEle.textContent = product.productPrice + ' Baht';
+        pProductPriceEle.setAttribute('class', 'card-text text-secondary');
+        divProductEle.appendChild(pProductPriceEle);
+        
+        const pProductStockEle = document.createElement('h6');
+        pProductStockEle.textContent = 'Available : ' + product.productStock;
+        pProductStockEle.setAttribute('class', 'card-text text-secondary');
+        divProductEle.appendChild(pProductStockEle);
+      
+        // const pProductBuyEle = document.createElement('button');
+        // pProductBuyEle.setAttribute('type', 'button');
+        // pProductBuyEle.setAttribute('class', 'btn btn-primary');
+        // pProductBuyEle.textContent = 'Buy Now';
+        // pProductBuyEle.onclick = function() {}
+        // divProductEle.appendChild(pProductBuyEle);
+      
+        divProductsEle.appendChild(divProductEle);
     }
-}
+})
+
 
 // function matchName(pName = '', sName= ''){
 //     return pName.toLowerCase().match(sName.toLowerCase());
