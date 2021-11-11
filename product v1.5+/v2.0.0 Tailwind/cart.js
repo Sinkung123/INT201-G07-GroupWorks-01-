c8bec7bcf91b16f1e1838b1169a7fa630e4d0929
Purachet Zheng <poorachad2545@gmail.com>
2021-11-12 03:39:54 +0700

    change framework to tailwind

---
+++ product v1.5+/v2.0.0 Tailwind/cart.js
@@ -0,0 +1,36 @@
+const countCartEle = document.getElementById('cart-count');
+const deleteCartBtnEle = document.getElementById('delete-cart');
+deleteCartBtnEle.addEventListener('click', removeCart);
+export let cart = [];
+
+export function addCart(e){
+    let id = e.target.parentNode.parentNode.id;
+    let x = cart.findIndex(cart => cart.productId == id);
+    // let x = cart.filter(cart => cart.productId == e.target.parentNode.id);
+    if(x == -1){
+        alert(`${id} added in your cart`);
+        cart.push({
+            productId: id,
+            // productName: product.productName,
+            // productPrice: product.productPrice,
+            qty: 1
+        })
+    }
+    else{
+        cart[x].qty++;
+    }
+    countCartEle.textContent = countCart();
+}
+
+export function removeCart() {
+    cart = [];
+    countCartEle.textContent = countCart();
+}
+
+export function countCart(){
+    let sum = 0;
+    cart.forEach(product => {sum += product.qty;})
+    return sum;
+}
+
+
