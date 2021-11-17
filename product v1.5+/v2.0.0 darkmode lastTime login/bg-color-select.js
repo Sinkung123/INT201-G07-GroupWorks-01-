9e67cc26045b3e51a1900639b54df38013fb7c92
Purachet Zheng <poorachad2545@gmail.com>
2021-11-17 20:12:50 +0700

    -

--- product v1.5+/v2.0.0 darkmode lastTime login/bg-color-select.js
+++ product v1.5+/v2.0.0 darkmode lastTime login/bg-color-select.js
@@ -1,14 +1,29 @@
 const bodyEle = document.getElementsByTagName('body')[0]
 const darkModeEle = document.getElementById('dark-mode')
 const whatModeEle = document.getElementById('modeNow')
+window.addEventListener('load', checkColorWhenOpen)
 darkModeEle.addEventListener('click', darkMode)
 
 
-bodyEle.style.backgroundColor = localStorage.getItem('bgColor');
+
+function checkColorWhenOpen(){
+    bodyEle.style.backgroundColor = localStorage.getItem('bgColor');
+    if(localStorage.getItem('bgColor') === 'white'|| localStorage.getItem('bgColor') === null){
+        darkModeEle.innerHTML = `<svg id="dark-mode" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>`
+    }
+    else{
+        darkModeEle.innerHTML = `<svg id="dark-mode" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+        </svg>`
+    }
+}
+
 
 
 function darkMode(){
-    if(localStorage.getItem('bgColor') === 'white'){
+    if(localStorage.getItem('bgColor') === 'white' || localStorage.getItem('bgColor') === null){
         bodyEle.style.backgroundColor = '#222';
         darkModeEle.innerHTML = `<svg id="dark-mode" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
         <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
@@ -18,7 +33,7 @@ function darkMode(){
     }
     else{
         bodyEle.style.backgroundColor = 'white';
-        darkModeEle.innerHTML = `<svg id="dark-mode" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        darkModeEle.innerHTML = `<svg id="dark-mode" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>`
 
--- product v1.5+/v2.0.0 darkmode lastTime login/last-time.js
+++ product v1.5+/v2.0.0 darkmode lastTime login/last-time.js
@@ -2,11 +2,11 @@ var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
+
 const lastTimeLoginEle = document.getElementById('last-time-login');
+
 if(localStorage.getItem('lastTimeLogin') !== null){
     lastTimeLoginEle.textContent = 'Last time: '+ localStorage.getItem('lastTimeLogin');
 }
-window.addEventListener('load', saveLastTime);
-function saveLastTime(){
-    localStorage.setItem('lastTimeLogin', dateTime)
-}
+
+window.addEventListener('load', () => localStorage.setItem('lastTimeLogin', dateTime));
