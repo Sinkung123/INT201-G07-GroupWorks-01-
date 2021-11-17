628b43b56d6fed0753d3b8d23012d08056516a6c
Purachet Zheng <poorachad2545@gmail.com>
2021-11-17 19:50:22 +0700

    cookie bgcolor

--- product v1.5+/v2.0.0 darkmode lastTime login/bg-color-select.js
+++ product v1.5+/v2.0.0 darkmode lastTime login/bg-color-select.js
@@ -3,23 +3,26 @@ const darkModeEle = document.getElementById('dark-mode')
 const whatModeEle = document.getElementById('modeNow')
 darkModeEle.addEventListener('click', darkMode)
 
-localStorage.setItem('bgColor', 'powderblue');
+
+bodyEle.style.backgroundColor = localStorage.getItem('bgColor');
 
 
 function darkMode(){
-    if(whatModeEle.value === 'light'){
+    if(localStorage.getItem('bgColor') === 'white'){
         bodyEle.style.backgroundColor = '#222';
         darkModeEle.innerHTML = `<svg id="dark-mode" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
         <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
         </svg>`
-        whatModeEle.value = 'dark'
+      
+        localStorage.setItem('bgColor', '#222');
     }
     else{
         bodyEle.style.backgroundColor = 'white';
         darkModeEle.innerHTML = `<svg id="dark-mode" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>`
-        whatModeEle.value = 'light'
+
+        localStorage.setItem('bgColor', 'white');
     }
     
 }
